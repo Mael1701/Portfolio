a2c114e9017393b5f0cfbe54a04405d6d58ffe03
Mael Micout <gh api micoutmael3@gmail.com>
2023-03-26 21:46:10 +0200

    header animation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+// Ajoutez ces lignes pour activer le support des fichiers CSS
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+environment.plugins.prepend(
+  'MiniCssExtractPlugin',
+  new MiniCssExtractPlugin({ filename: 'css/[name]-[contenthash].css' })
+);
+environment.loaders.prepend('css', {
+  test: /\.css$/,
+  use: [MiniCssExtractPlugin.loader, 'css-loader'],
+});
+
+module.exports = environment;
+
