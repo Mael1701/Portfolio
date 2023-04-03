26557356106b0458369340f323b33bf872db5b7f
Mael Micout <gh api micoutmael3@gmail.com>
2023-04-03 21:38:41 +0200

    Updating production mailer settings to use MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,14 @@
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://portfolio-mael-micout.herokuapp.com/', # UPDATE THIS VALUE WITH YOUR OWN APP
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
-
-
 require "active_support/core_ext/integer/time"
 
-
+ ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'https://portfolio-mael-micout.herokuapp.com/', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
